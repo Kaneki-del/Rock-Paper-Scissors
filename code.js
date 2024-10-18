ea7d7cca81886f283609c66f536d7a4bb5b914c2
soufiane <sofiansofianait@gmail.com>
2024-10-18 17:31:15 +0100

    Final result

--- code.js
+++ code.js
@@ -9,53 +9,58 @@ function getComputerChoicee() {
         return "Rock";
 }
 
-const formatChoice = (choice) => choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-
-function getHumanChoicee() {
-    let choice = prompt("Choose: Rock, Paper, or Scissor"); // Get user input
-    return formatChoice(choice); // Use the formatChoice function to format the input
+var Rock = document.querySelector(".Rock");
+var Paper = document.querySelector(".Paper");
+var Scissor = document.querySelector(".Scissor");function getComputerChoicee() {
+    let random;
+    random = Math.floor(Math.random() * 3);
+    if (random === 0)
+        return "Paper";
+    else if (random == 1)
+        return "Scissor";
+    else
+        return "Rock";
 }
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(ComputerChoice, HumanChoice) {
+var humanScore = 0;
+var computerScore = 0;
+function playRound(HumanChoice) {
+    var ComputerChoice = getComputerChoicee();
     let pc = "You lose! " + ComputerChoice + " beats " + HumanChoice;
     let human = "You win! " + HumanChoice + " beats " + ComputerChoice;
-    if (ComputerChoice === HumanChoice)
-        console.log("It's a draw! you bouth chose " + ComputerChoice);
-    else if (ComputerChoice === "Paper" && HumanChoice === "Rock") {
-        console.log(pc);
+
+    if (ComputerChoice === HumanChoice) {
+        result.innerHTML = "It's a draw! You both chose " + ComputerChoice;
+    } else if (
+        (ComputerChoice === "Paper" && HumanChoice === "Rock") || 
+        (ComputerChoice === "Rock" && HumanChoice === "Scissor") || 
+        (ComputerChoice === "Scissor" && HumanChoice === "Paper")
+    ) {
         computerScore++;
-    } else if (ComputerChoice === "Paper" && HumanChoice === "Scissor") {
-        console.log(human);
+        result.innerHTML = `${pc}: You have ${humanScore} points, and computer has ${computerScore} points.`;
+    } else {
         humanScore++;
-    } else if (ComputerChoice === "Rock" && HumanChoice === "Paper") {
-        console.log(human);
-        humanScore++;
-    } else if (ComputerChoice === "Rock" && HumanChoice === "Scissor") {
-        console.log(pc);
-        computerScore++;
-    } else if (ComputerChoice === "Scissor" && HumanChoice === "Paper") {
-        console.log(pc);
-        computerScore++;
-    } else if (ComputerChoice === "Scissor" && HumanChoice === "Rock") {
-        console.log(human);
-        humanScore++;
+        result.innerHTML = `${human}: You have ${humanScore} points, and computer has ${computerScore} points.`;
     }
+    if (humanScore === 5){
+        result.innerHTML = "Congratulations! You are the winner! 🎉";
+        resetGame();
+    }else if ( computerScore === 5){
+        result.innerHTML = "Sorry! The computer is the winner! 😢";
+        resetGame();
+    }"Sorry! The computer is the winner! 😢"
+}
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
 }
 
-function playGame() {
-    let i = 0;
-    while (i < 5) {
-        const humanSelection = getHumanChoicee();
-        const computerSelection = getComputerChoicee();
-        playRound(computerSelection, humanSelection);
-        i++;
-    }
-    if (humanScore === computerScore) {
-        console.log("You have a draw");
-    } else if (humanScore > computerScore) {
-        console.log("You are the winner");
-    } else
-        console.log("The computer is the winner");
-}
+var Rock = document.querySelector(".Rock");
+var Paper = document.querySelector(".Paper");
+var Scissor = document.querySelector(".Scissor");
+var result = document.querySelector(".result");
+
+
+Rock.addEventListener("click", () => playRound("Rock"));
+Paper.addEventListener("click", () => playRound("Paper"));
+Scissor.addEventListener("click", () => playRound("Scissor"));
